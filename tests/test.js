f1bdd85da05d46adfeddb10ad41b2d75d417d639
Jodu555 <Jodu505@gmail.com>
2022-03-25 12:28:59 +0100

    Ported everything into here

---
+++ tests/test.js
@@ -0,0 +1,13 @@
+const Logger = require('../src/Logger');
+
+const path = require('path');
+
+
+const logger = new Logger(path.join(process.cwd(), 'latest.log'));
+logger.setLevel(-1);
+
+logger.fatal('123: fatal');
+logger.error('123: error');
+logger.warn('123: warn');
+logger.info('123: info');
+logger.debug('123: debug');
